6a0728aeaea77e1ff55f95fe327e30fa30e177a2
Damien-Grenoulaud <137390816+Damien-Grenoulaud@users.noreply.github.com>
2023-06-22 16:26:31 +0200

    version 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,4 +9,3 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'welcome#index'
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts "Seeding..."
+
+Article.destroy_all
+
+for n in 0..50
+    @article = Article.create([title: Faker::Name.name,text: Faker::Markdown.emphasis])
+    for y in 0..3
+        Comment.create([article_id: @article.first.id,commenter: Faker::Name.name,body: Faker::Markdown.emphasis])
+        
+
+    end
+    puts Faker::Name.name
+end
+
+puts "Seeding done."
