94f2ebad9168b9af74005f96baa712a01ee5fa54
Damien-Grenoulaud <137390816+Damien-Grenoulaud@users.noreply.github.com>
2023-07-13 10:44:21 +0200

    Version 9

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -9,4 +9,11 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template :index
   end
+
+  test 'should get new' do
+    get new_article_path
+
+    assert_response :success,"Acces au new"
+    assert_template :new,"Acces au new"
+  end
 end
