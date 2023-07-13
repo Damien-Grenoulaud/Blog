b9b81e1601b89f0772250a6e1c427ad998a8f40e
Damien-Grenoulaud <137390816+Damien-Grenoulaud@users.noreply.github.com>
2023-07-13 09:55:57 +0200

    Version 7

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -3,6 +3,15 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
+  test "Creation titre mauvais" do
+    @article = Article.new(title: "test")
+    assert_not @article.save,"test"
+  end
+
+  test "Creation titre bon" do
+    @article = Article.new(title: "test bon")
+    assert @article.save,"test bon"
+  end
   # test "the truth" do
   #   assert true
   # end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,6 +3,10 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
+  test "Creation Commentaire sans article" do
+    @Comment = Comment.new
+    assert_not @Comment.save,"test mauvais"
+  end
   # test "the truth" do
   #   assert true
   # end
