db0c43567546e55978d14b4f9f1164cb7086adc1
vianney.sonneville <vianney.sonneville@unova.fr>
2023-07-18 17:06:41 +0200

    update role

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,4 +14,12 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+
+  def editable?
+    true
+  end
+
+  def deletable?
+    true
+  end
 end
