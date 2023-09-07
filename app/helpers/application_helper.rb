baf961dfce21043311e5fb2967e9cb13d07b097e
Damien-Grenoulaud <137390816+Damien-Grenoulaud@users.noreply.github.com>
2023-09-07 10:18:50 +0200

    Rubocop Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,18 +2,22 @@
 
 module ApplicationHelper
   def flash_success(&)
-    tag.div class: 'flex items-center justify-between p-4 text-white', style: 'background-color: cadetblue !important', 'data-controller': 'alert' do
-      concat tag.div "✓", class: "mx-2 my-2 text-lg h-10 w-10 border-2 rounded-full flex items-center justify-center border-white"
-      concat tag.div(capture(&), class: "alert-body")
-      concat tag.div "x", class: "cursor-pointer", 'data-action': "click->alert#close"
+    tag.div class: 'flex items-center justify-between p-4 text-white', style: 'background-color: cadetblue !important',
+            'data-controller': 'alert' do
+      concat tag.div '✓',
+                     class: 'mx-2 my-2 text-lg h-10 w-10 border-2 rounded-full flex items-center justify-center border-white'
+      concat tag.div(capture(&), class: 'alert-body')
+      concat tag.div 'x', class: 'cursor-pointer', 'data-action': 'click->alert#close'
     end
   end
 
   def flash_error(&)
-    tag.div class: 'flex items-center justify-between p-4 text-white', style: 'background-color: darkred !important', 'data-controller': 'alert' do
-      concat tag.div "✓", class: "mx-2 my-2 text-lg h-10 w-10 border-2 rounded-full flex items-center justify-center border-white"
-      concat tag.div(capture(&), class: "alert-body")
-      concat tag.div "x", class: "cursor-pointer", 'data-action': "click->alert#close"
+    tag.div class: 'flex items-center justify-between p-4 text-white', style: 'background-color: darkred !important',
+            'data-controller': 'alert' do
+      concat tag.div '✓',
+                     class: 'mx-2 my-2 text-lg h-10 w-10 border-2 rounded-full flex items-center justify-center border-white'
+      concat tag.div(capture(&), class: 'alert-body')
+      concat tag.div 'x', class: 'cursor-pointer', 'data-action': 'click->alert#close'
     end
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 module ArticlesHelper
-    def verif_status_actif(status)
-        if status.actif?
-            "bg-lime-400"
-        else
-            "bg-neutral-400"
-        end
+  def verif_status_actif(status)
+    if status.actif?
+      'bg-lime-400'
+    else
+      'bg-neutral-400'
     end
-    def verif_status_inactif(status)
-        if status.inactif?
-            "bg-red-600"
-        else
-            "bg-neutral-400"
-        end
+  end
+
+  def verif_status_inactif(status)
+    if status.inactif?
+      'bg-red-600'
+    else
+      'bg-neutral-400'
     end
+  end
 end
